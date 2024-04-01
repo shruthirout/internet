9e61ca06ca200ce6702ddccb2810e3ef3747fb15
shruthirout <shruthirout05@gmail.com>
2024-04-01 12:48:52 +0530

    practice

---
+++ expression.js
@@ -0,0 +1,23 @@
+//arithmetic operations
+let sum = 1 + 1.4; // 2.4
+let dif = 1 - 1.4; // -0.4
+let mul = 2 * 4; // 8
+let div = 4 / 2; // 2
+let mod = 5 % 2; // 1
+let pow = 5 ** 2; // 25
+let ex1 = 2 * (1 - 6 / 2); // -4
+//boolean
+let isMore = 2 >= 4; // false
+let isLess = 5 < 10; // true
+let bool1 = isMore || isLess; // true
+let bool2 = isMore && isLess; // false
+let isEqual1 = 0 == '0';//true type conversion
+let isEqual2 = 0 === '0'; //false no type conversion
+let notEqual1 = 0 != '0'; //false
+let notEqual2 = 0 !== '0'; //true
+//string
+let str1 = 'Hello';
+let str2 = 'World';
+let str3 = str1 + ' ' + str2;//'Hello World'
+let num = 42;
+let str4 = `The answer to universe is ${num}.`;//The answer to universe is 42.
